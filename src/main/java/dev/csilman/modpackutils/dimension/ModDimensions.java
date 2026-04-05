83a456386d7e8f9c9991f6396240230448829744
Chris S. <christopherjsilman@gmail.com>
2026-04-05 11:35:42 -0400

    Added simple structure generation in the overworld, and start of Dimensions class.

---
+++ src/main/java/dev/csilman/modpackutils/dimension/ModDimensions.java
@@ -0,0 +1,14 @@
+package dev.csilman.modpackutils.dimension;
+
+import dev.csilman.modpackutils.ModpackUtilsMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
+
+public class ModDimensions {
+    public static final ResourceKey<Level> TEST_WORLD = ResourceKey.create(
+            Registries.DIMENSION,
+            ResourceLocation.fromNamespaceAndPath(ModpackUtilsMod.MOD_ID, "test_world")
+    );
+}
